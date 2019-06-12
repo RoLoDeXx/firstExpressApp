4cc0abb7d5da97ea8c5e615e8b1fc4cf28acf39a
RoLoDeXx <dexxrolo@gmail.com>
2019-06-13 00:14:41 +0530

    final changes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,7 @@ const forcast = require("./utils/forcast");
 const geocode = require("./utils/geocode");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 app.set("view engine", "hbs"); //set view engine aka template engine
 
